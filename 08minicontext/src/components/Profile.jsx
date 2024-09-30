afbb6b3b702c84fab0feffeb6eab93eb5bc10428
rai252 <khushirai4002@gmail.com>
2024-09-30 11:49:47 +0530

    "Remove comment and update setUser call in Profile component"

--- 08minicontext/src/components/Profile.jsx
+++ 08minicontext/src/components/Profile.jsx
@@ -10,7 +10,7 @@ function Profile() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Update user information (if required)
+
         setUser({ username, password });
     };
 
